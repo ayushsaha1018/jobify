f3aa3e77014f428f614bab042fbbe576549d1150
Ayush Kumar <ayushsaha111@gmail.com>
2022-08-22 15:22:43 +0530

    Monthly Applications

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -6,6 +6,7 @@ import {
   NotFoundError,
   UnAuthenticatedError,
 } from "../errors/index.js";
+import moment from "moment";
 import checkPermissions from "../utils/checkPermissions.js";
 
 const createJob = async (req, res) => {
@@ -81,7 +82,30 @@ const showStats = async (req, res) => {
     interview: stats.interview || 0,
     declined: stats.declined || 0,
   };
-  let monthlyApplications = [];
+
+  let monthlyApplications = await Job.aggregate([
+    { $match: { createdBy: mongoose.Types.ObjectId(req.user.userId) } },
+    {
+      $group: {
+        _id: { year: { $year: "$createdAt" }, month: { $month: "$createdAt" } },
+        count: { $sum: 1 },
+      },
+    },
+    { $sort: { "_id.year": -1, "_id.month": -1 }  },
+    { $limit: 6 },
+  ]);
+  monthlyApplications = monthlyApplications.map((item) => {
+    const {
+      _id: { year, month },
+      count,
+    } = item;
+    const date = moment()
+      .month(month - 1)
+      .year(year)
+      .format("MMM Y");
+    return { date, count };
+  }).reverse();
+
   res.status(StatusCodes.OK).json({ defaultStats, monthlyApplications });
 };
 
