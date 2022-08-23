9bb6faf64b42aa43be64f1e1e26005402b9fcce3
Ayush Kumar <ayushsaha111@gmail.com>
2022-08-23 14:48:04 +0530

    Adding security packages

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,7 +1,6 @@
 import { StatusCodes } from "http-status-codes";
 
 const errorHandlerMiddleware = (err, req, res, next) => {
-  console.log(err.message);
   const defaultError = {
     statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
     msg: err.message || "Something went wrong, try again later",
@@ -17,7 +16,6 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     defaultError.statusCode = StatusCodes.BAD_REQUEST;
     defaultError.msg = `${Object.keys(err.keyValue)} field has to be unique`;
   }
-  // res.status(defaultError.statusCode).json({ msg: err });
   res.status(defaultError.statusCode).json({ msg: defaultError.msg });
 };
 
--- server.js
+++ server.js
@@ -2,8 +2,15 @@ import express from "express";
 const app = express();
 import dotenv from "dotenv";
 dotenv.config();
-import "express-async-errors"
+import "express-async-errors";
 import morgan from "morgan";
+import helmet from "helmet";
+import xss from "xss-clean";
+import mongoSanitize from "express-mongo-sanitize";
+
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import path from "path";
 
 // db and authenticate user
 import connectDB from "./db/connect.js";
@@ -15,27 +22,39 @@ import jobsRouter from "./routes/jobsRoutes.js";
 // middleware
 import notFoundMiddleware from "./middleware/not-found.js";
 import errorHandlerMiddleware from "./middleware/error-handler.js";
-import authenticateUser from "./middleware/auth.js"
+import authenticateUser from "./middleware/auth.js";
 
-if(process.env.NODE_ENV !== "production"){
-  app.use(morgan("dev"))
+if (process.env.NODE_ENV !== "production") {
+  app.use(morgan("dev"));
 }
- 
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+
 app.use(express.json());
-app.get("/", (req, res) => { 
-  res.json({msg: "Welcome"});
-});
 
-app.get("/api/v1", (req, res) => { 
-  res.json({msg: "API"});
-});
-
+app.use(helmet());
+app.use(xss());
+app.use(mongoSanitize());
 
 app.use("/api/v1/auth", authRouter);
-app.use("/api/v1/jobs",authenticateUser, jobsRouter);
+app.use("/api/v1/jobs", authenticateUser, jobsRouter);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
+
+app.get("/", (req, res) => {
+  res.json({ msg: "Welcome" });
+});
+
+app.get("/api/v1", (req, res) => {
+  res.json({ msg: "API" });
+});
 
 const port = process.env.PORT || 5000;
 
